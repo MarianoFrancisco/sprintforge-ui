c6d7cbdc6aa165c3a66ca3f59a8ef43e1b38312d
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-12-07 00:48:18 -0600

    feat: add dark theme and login page

---
+++ ui/app/components/login-form.tsx
@@ -0,0 +1,91 @@
+import { cn } from "~/lib/utils"
+import { Button } from "~/components/ui/button"
+import { Card, CardContent } from "~/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from "~/components/ui/field"
+import { Input } from "~/components/ui/input"
+import { ModeToggle } from "./ui/mode-toggle"
+import { Link } from "react-router"
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8">
+            <FieldGroup>
+                <ModeToggle />
+              <div className="flex flex-col items-center gap-2 text-center">
+                <h1 className="text-2xl font-bold">Bienvenido</h1>
+                <p className="text-muted-foreground text-balance">
+                  Inicia sesión en tu cuenta de Sprint Forge
+                </p>
+              </div>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Contraseña</FieldLabel>
+                  <Link
+                    to="#"
+                    className="ml-auto text-sm underline-offset-2 hover:underline"
+                  >
+                    ¿Olvidaste tu contraseña?
+                  </Link>
+                </div>
+                <Input id="password" type="password" required />
+              </Field>
+              <Field>
+                <Button type="submit">Iniciar Sesión</Button>
+              </Field>
+              {/* <FieldDescription className="text-center">
+                Sin cuenta? <Link to="#">Crear cuenta</Link>
+              </FieldDescription> */}
+            </FieldGroup>
+          </form>
+<div className="relative hidden md:block bg-muted">
+  {/* Imagen normal */}
+  <img
+    src="/mountains.jpg"
+    alt="Background"
+    className="absolute inset-0 h-full w-full object-cover"
+  />
+
+  {/* Capa enmascarada con el logo y efecto naranja */}
+  <div
+    className="
+      absolute inset-0 h-full w-full 
+      mask-[url('/logo.svg')] mask-no-repeat mask-center mask-contain
+      bg-[url('/mountains.jpg')] bg-cover bg-center
+      mix-blend-multiply
+    "
+  />
+</div>
+
+
+
+
+        </CardContent>
+      </Card>
+      {/* <FieldDescription className="px-6 text-center">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </FieldDescription> */}
+    </div>
+  )
+}
--- ui/app/root.tsx
+++ ui/app/root.tsx
@@ -5,10 +5,14 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  type LoaderFunctionArgs,
 } from "react-router";
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { themeSessionResolver } from "./sessions.server";
+import { PreventFlashOnWrongTheme, ThemeProvider, useTheme } from "remix-themes";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -21,19 +25,44 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+      // Favicons
+  { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+  { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+  { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+  { rel: "icon", href: "/favicon.ico" },
 ];
 
-export function Layout({ children }: { children: React.ReactNode }) {
+
+// Return the theme from the session storage using the loader
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { getTheme } = await themeSessionResolver(request)
+  return {
+    theme: getTheme(),
+  }
+}
+
+function App() {
+  const data = useLoaderData();
+  const [theme] = useTheme();
+
+const className =
+  theme === "dark"
+    ? "dark"
+    : theme === "light"
+    ? ""
+    : ""; // System media query
+
   return (
-    <html lang="en">
+    <html lang="en" data-theme={theme ?? ""} className={className ?? ""}>
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
+        <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
         <Links />
       </head>
       <body>
-        {children}
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -41,8 +70,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
-export default function App() {
-  return <Outlet />;
+export default function AppWithProviders() {
+  const data = useLoaderData();
+  return (
+    <ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme" disableTransitionOnThemeChange={true}>
+      <App />
+    </ThemeProvider>
+  )
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
