364104c124290d826dbce28e126a38a971b40f36
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-12-13 00:02:47 -0600

    fix: ssr mode toggle theme

--- ui/app/components/ui/mode-toggle.tsx
+++ ui/app/components/ui/mode-toggle.tsx
@@ -1,26 +1,41 @@
-import { Monitor, Moon, Sun } from "lucide-react"
-import { Theme, useTheme } from "remix-themes"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./dropdown-menu"
-import { Button } from "./button"
+import { useEffect, useState } from "react";
+import { Monitor, Moon, Sun } from "lucide-react";
+import { Theme, useTheme } from "remix-themes";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from "./dropdown-menu";
+import { Button } from "./button";
 
 export function ModeToggle() {
   const [theme, setTheme] = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const renderIcon = () => {
+    if (!mounted) return <Monitor className="h-[1.2rem] w-[1.2rem]" />;
+
+    return theme === Theme.DARK ? (
+      <Moon className="h-[1.2rem] w-[1.2rem]" />
+    ) : (
+      <Sun className="h-[1.2rem] w-[1.2rem]" />
+    );
+  };
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon">
-          {/* <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" /> */}
-          {theme === Theme.DARK ? (
-            <Moon className="h-[1.2rem] w-[1.2rem]" />
-          ) : theme === Theme.LIGHT ? (
-            <Sun className="h-[1.2rem] w-[1.2rem]" />
-          ) : 
-          <Monitor className="h-[1.2rem] w-[1.2rem]" />}
+        <Button variant="ghost" size="icon" type="button">
+          {renderIcon()}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
+
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme(Theme.LIGHT)}>
           Claro
@@ -33,5 +48,5 @@ export function ModeToggle() {
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
