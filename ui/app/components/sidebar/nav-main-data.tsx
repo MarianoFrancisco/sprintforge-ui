0b42ff86267760e30b6ffe19692eefb4cfed40d4
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-12-27 14:56:12 -0600

    feat: edit permissions ui

--- ui/app/components/sidebar/nav-main-data.tsx
+++ ui/app/components/sidebar/nav-main-data.tsx
@@ -7,32 +7,32 @@ export const navMainData: NavItem[] = [
     title: "Puestos",
     url: "/employees/positions",
     icon: Users,
-    permission: PERMS.POSITION_LIST, // para ver el módulo
+    permission: PERMS.POSITION_VIEW, // para ver el módulo
     items: [
       { title: "Nuevo puesto", url: "/employees/positions/create", permission: PERMS.POSITION_CREATE },
-      { title: "Listado", url: "/employees/positions", permission: PERMS.POSITION_LIST },
+      { title: "Listado", url: "/employees/positions", permission: PERMS.POSITION_VIEW },
     ],
   },
   {
     title: "Empleados",
     url: "/employees",
     icon: BriefcaseBusiness,
-    permission: PERMS.EMPLOYEE_LIST,
+    permission: PERMS.EMPLOYEE_VIEW,
     items: [
-      { title: "Contratar empleado", url: "/employees/hire", permission: PERMS.EMPLOYEE_CREATE },
-      { title: "Listado", url: "/employees", permission: PERMS.EMPLOYEE_LIST },
-      { title: "Historial laboral", url: "/employees/history" },
-      { title: "Historial de pagos", url: "/employees/payments" },
+      { title: "Contratar empleado", url: "/employees/hire", permission: PERMS.EMPLOYEE_HIRE },
+      { title: "Listado", url: "/employees", permission: PERMS.EMPLOYEE_VIEW },
+      { title: "Historial laboral", url: "/employees/history", permission: PERMS.EMPLOYEE_VIEW_WORK_HISTORY },
+      { title: "Historial de pagos", url: "/employees/payments", permission: PERMS.EMPLOYEE_VIEW_PAYMENT_HISTORY },
     ],
   },
   {
     title: "Roles",
     url: "/identity/roles",
     icon: BriefcaseBusiness,
-    permission: PERMS.ROLE_LIST,
+    permission: PERMS.ROLE_VIEW,
     items: [
       { title: "Crear rol", url: "/identity/roles/create", permission: PERMS.ROLE_CREATE },
-      { title: "Listado", url: "/identity/roles", permission: PERMS.ROLE_LIST },
+      { title: "Listado", url: "/identity/roles", permission: PERMS.ROLE_VIEW },
     ],
   },
 ];
